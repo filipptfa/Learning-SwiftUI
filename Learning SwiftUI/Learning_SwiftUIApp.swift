ec732d67aeee432a0789d9632ac36b933f4814ae
filipptfa <filipptfa@gmail.com>
2024-03-04 21:45:35 +0200

    Initial Commit

---
+++ Learning SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning SwiftUI
+//
+//  Created by Pylyp Chulanov on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
