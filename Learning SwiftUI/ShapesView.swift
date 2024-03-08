9d612e8ea9831fc1f6818ada473400951ad512d8
filipptfa <filipptfa@gmail.com>
2024-03-08 18:01:56 +0200

    8.03.2024

---
+++ Learning SwiftUI/ShapesView.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesView.swift
+//  Learning SwiftUI
+//
+//  Created by Pylyp Chulanov on 05.03.2024.
+//
+
+import SwiftUI
+
+struct ShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesView()
+}
---
+++ Learning SwiftUI/styles/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  Learning SwiftUI
+//
+//  Created by Pylyp Chulanov on 08.03.2024.
+//
+
+import Foundation
