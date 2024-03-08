808ba1dd419b5a4ca451b5b1e88ba1f4525d40fe
filipptfa <filipptfa@gmail.com>
2024-03-08 22:44:57 +0200

    lesson 5

---
+++ Learning SwiftUI/05.swift
@@ -0,0 +1,18 @@
+//
+//  05.swift
+//  Learning SwiftUI
+//
+//  Created by Pylyp Chulanov on 08.03.2024.
+//
+
+import SwiftUI
+
+struct _5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _5()
+}
